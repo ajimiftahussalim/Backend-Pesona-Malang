914e6c8cf8a39788f7472bc8a9d46ca39cbc9a30
Eko Rahayu Widodo <100954726+ekorahy@users.noreply.github.com>
2022-06-30 02:15:02 +0700

    Delete Fitur Signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import Home from './Home';
 import Login from './Login';
 import MainApp from './MainApp';
-import Signup from './SignUp';
 import CreateTour from './CreateTour';
 import DetailTour from './DetailTour';
 
-export { Home, Login, MainApp, Signup, CreateTour, DetailTour };
+export { Home, Login, MainApp, CreateTour, DetailTour };
